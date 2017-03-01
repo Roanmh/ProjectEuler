7180cffaec5c61285299267d3f8cbf052b751a39
roanmh <roanmh@gmail.com>
2017-02-28 19:31:16 -0500

    made tests for brute method, untested (full run takes to long)

--- Euler Projects/EulerProject15.py
+++ Euler Projects/EulerProject15.py
@@ -2,7 +2,16 @@
 #  Auth: Roan Martin-Hayden
 #  Date: 12/28/2016
 
+import getopt
+import sys
+import progressbar
+
+bar = progressbar.ProgressBar(maxval=20 ** 20).start()
+i = 0
+
+
 def recursive(height, width):
+    global i
     h_result = 0
     w_result = 0
     if height > 0:
@@ -10,5 +19,30 @@ def recursive(height, width):
     if width > 0:
         w_result = recursive(height, width - 1)
     if width == 0 or height == 0:
+        i += 1
+        bar.update(i)
         return 1
+    i += 1
+    bar.update(i)
     return h_result + w_result
+
+
+def main(argv):
+    height = 20
+    width = 20
+    try:
+        opts, argv = getopt.getopt(argv, 'w:h:')
+    except getopt.GetoptError:
+        print("I failed you")
+        sys.exit(2)
+
+    for opt, arg in opts:
+        if opt == "-h":
+            height = arg
+        if opt == "-w":
+            width = arg
+
+    print(recursive(height, width))
+
+if __name__ == '__main__':
+    main(sys.argv)
--- Euler Projects/Tests/15Tests.py
+++ Euler Projects/Tests/15Tests.py
@@ -18,5 +18,20 @@ class MyTestCase(unittest.TestCase):
     def test_h_zero_w_two_returns_one(self):
         self.assertEqual(1, recursive(0,2))
 
+    def test_h_two_w_one_returns_one(self):
+        self.assertEqual(3, recursive(2,1))
+
+    def test_h_one_w_two_returns_one(self):
+        self.assertEqual(3, recursive(1,2))
+
+    def test_h_two_w_two_returns_one(self):
+        self.assertEqual(6, recursive(2,2))
+
+    def test_h_two_w_three_returns_one(self):
+        self.assertEqual(10, recursive(2,3))
+
+    def test_h_three_w_two_returns_one(self):
+        self.assertEqual(10, recursive(3,2))
+
 if __name__ == '__main__':
     unittest.main()
